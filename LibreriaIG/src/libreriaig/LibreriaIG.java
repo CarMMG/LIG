8c11e57fa12fe5cebed9c7b173ba986f89c0d1b5
Carlos Moises <carlosmoymg@gmail.com>
2024-02-01 16:43:46 -0600

    se crea el menu primcipal de opciones

--- LibreriaIG/src/libreriaig/LibreriaIG.java
+++ LibreriaIG/src/libreriaig/LibreriaIG.java
@@ -4,17 +4,93 @@
  */
 package libreriaig;
 
+
 /**
  *
  * @author moyme
  */
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class LibreriaIG {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JFrame frame = new JFrame("Librería");
+        frame.setSize(400, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton btnAgregar = new JButton("Agregar");
+        JButton btnModificar = new JButton("Modificar");
+        JButton btnConsultar = new JButton("Consultar");
+        JButton btnVender = new JButton("Vender");
+        JButton btnSalir = new JButton("Salir");
+
+        frame.getContentPane().setLayout(null);
+        btnAgregar.setBounds(30, 30, 100, 30);
+        btnModificar.setBounds(150, 30, 100, 30);
+        btnConsultar.setBounds(270, 30, 100, 30);
+        btnVender.setBounds(90, 80, 100, 30);
+        btnSalir.setBounds(210, 80, 100, 30);
+
+        frame.getContentPane().add(btnAgregar);
+        frame.getContentPane().add(btnModificar);
+        frame.getContentPane().add(btnConsultar);
+        frame.getContentPane().add(btnVender);
+        frame.getContentPane().add(btnSalir);
+
+        btnAgregar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame agregarFrame = new JFrame("Agregar");
+                agregarFrame.setSize(300, 200);
+                agregarFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                agregarFrame.setVisible(true);
+            }
+        });
+
+        btnModificar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame modificarFrame = new JFrame("Modificar");
+                modificarFrame.setSize(300, 200);
+                modificarFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                modificarFrame.setVisible(true);
+            }
+        });
+
+        btnConsultar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame consultarFrame = new JFrame("Consultar");
+                consultarFrame.setSize(300, 200);
+                consultarFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                consultarFrame.setVisible(true);
+            }
+        });
+
+        btnVender.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame venderFrame = new JFrame("Vender");
+                venderFrame.setSize(300, 200);
+                venderFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                venderFrame.setVisible(true);
+            }
+        });
+
+        btnSalir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        frame.setVisible(true);
     }
-    
 }
